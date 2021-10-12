a702bde2021557a0a4d20c97fbdc4ecbd792e786
khanh <duykhanh.longcang@gmail.com>
2021-10-12 07:46:15 -0700

    prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -2,28 +2,23 @@ const express = require('express');
 const path = require('path');
 // const sass = require('sass');
 const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined'));
 
 //template engine
-app.engine('hbs', handlebars(
-  {extname: '.hbs',}
-));
+app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {   
-  res.render('home');
-})
-app.get('/new', (req, res) => {   
-  res.render('new');
-})
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
